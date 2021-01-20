f68e83258b2cf43e9d83aeef0820e6952686d084
Ryan DeJana <rdejana@us.ibm.com>
2021-01-20 11:10:39 -0700

    examples

--- pubsub/client.py
+++ pubsub/client.py
@@ -5,7 +5,7 @@ import os
 MQTT_BROKER = os.getenv('MQTT')
 
 client = mqtt.Client("P1") #create new instance
-client.connect(broker_address) #connect to broker
+client.connect(MQTT_BROKER) #connect to broker
 client.publish("example","Hello World")#publish
 
 print("send done")
